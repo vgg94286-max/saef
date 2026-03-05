3776039383602c0708c506ac3312909e053c3f20
abaduy123 <dgikpp@gmail.com>
2026-03-05 12:35:57 +0300

    first commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,10 @@
 import postgres, { Sql } from "postgres";
 
-export const sql: Sql = postgres(process.env.NEON_URL!);
+export const sql: Sql = postgres(process.env.NEON_URL!, {
+  ssl: 'require',
+  connect_timeout: 30,
+  // This forces the session to use UTF8 for Arabic support
+  onparameter: (name, value) => {
+    if (name === 'client_encoding') return 'UTF8';
+  }
+});
